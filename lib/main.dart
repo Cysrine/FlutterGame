98b2b325de9a30d18d16a7a151ae4caeae276d3d
Nicolas-Ste <3120316@stud.srh-campus-berlin.de>
2025-02-15 17:47:11 +0100

    Homepage creation

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final game = MyGame(); // Create our Flame game instance
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: GameWidget(
-          game: game,
-        ),
+      home: const MainMenu(),
+    );
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Change: Background image added
+          Positioned.fill(
+            child: Image.asset('assets/pixelgalaxy.jpg',fit: BoxFit.cover,),
+          ),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Asteroid Shooter',
+                  style: TextStyle(fontSize: 32, color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => GameScreen(),
+                      ),
+                    );
+                  },
+                  child: const Text('Start Game'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  GameScreen({super.key});
+
+  final MyGame game = MyGame(); // Creating the game instance
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GameWidget(game: game),
     );
   }
 }
